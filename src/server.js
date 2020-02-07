f582ef6c7355c0d63304ce773a693b083684ae12
Erik Hoffman <erik.johansson87@gmail.com>
2020-02-07 08:34:23 +0100

    Added rate limit plugin

--- src/server.js
+++ src/server.js
@@ -3,6 +3,13 @@ const fastify = require("fastify")({
 });
 const cors = require("cors");
 fastify.use(cors());
+
+const fastifyRateLimit = require("fastify-rate-limit");
+fastify.register(fastifyRateLimit, {
+  max: 100,
+  timeWindow: "1 minute"
+});
+
 fastify.register(require("./routes/position"), {
   prefix: "/position"
 });
