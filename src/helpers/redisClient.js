3b69829a5d80169519d43a3cdc5bfd9518961564
Erik Hoffman <erik.johansson87@gmail.com>
2020-02-13 07:58:25 +0100

    changed redis package to ioredis for native promise support

--- src/helpers/redisClient.js
+++ src/helpers/redisClient.js
@@ -1,21 +1,10 @@
-const redis = require("redis");
-const promisifyAll = require("util-promisifyall");
+const Redis = require("ioredis");
 
 const config = {
-  redisUrl: process.env.REDIS_URL || "127.0.0.1",
-  redisPort: process.env.REDIS_PORT || 6379,
-  redisAuth: process.env.REDIS_AUTH || ""
+  port: process.env.REDIS_PORT || 6379,
+  host: process.env.REDIS_URL || "127.0.0.1",
+  passowrd: process.env.REDIS_AUTH || ""
 };
 
-promisifyAll(redis.RedisClient.prototype);
-
-const client = redis.createClient(config.redisPort, config.redisUrl);
-if (config.redisAuth) {
-  client.auth(config.redisAuth, err => {
-    if (err) {
-      throw err;
-    }
-  });
-}
-
+const client = new Redis(config);
 module.exports = client;
