6f71f23bb58e90b68eaa520afb9fc3886c82e9e3
Jonas Birmé <jonas.birme@eyevinn.se>
2024-02-29 07:48:33 +0100

    fix: authentication (#14)

--- src/helpers/redisClient.js
+++ src/helpers/redisClient.js
@@ -3,7 +3,8 @@ const Redis = require("ioredis");
 const config = {
   port: process.env.REDIS_PORT || 6379,
   host: process.env.REDIS_URL || "127.0.0.1",
-  passowrd: process.env.REDIS_AUTH || ""
+  username: process.env.REDIS_USERNAME || "",
+  password: process.env.REDIS_PASSWORD || ""
 };
 
 const client = new Redis(config);
